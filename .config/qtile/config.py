cd1901b64ad024521f0275ea610e26fa4e336dab
Daniel Wee <danielweedk@gmail.com>
2025-02-12 17:19:39 +0800

    Fixed bug
    Ghidra mannn

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -764,8 +764,8 @@ wl_xcursor_size = 24
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-# wmname = "LG3D"
-wmname = f"Qtile {VERSION}"
+wmname = "LG3D"
+# wmname = f"Qtile {VERSION}"
 
 
 @hook.subscribe.startup_once
