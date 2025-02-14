9c0ebd05eba46c377753e44a7f04ea5574f76ad6
Daniel Wee <danielweedk@gmail.com>
2025-02-14 22:53:21 +0800

    Added virt-manager stuff

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -156,6 +156,7 @@ keys = [
     Key([mod, "shift"], "w", lazy.spawn("wasistlos"), desc="Launch Whatsapp for Linux"),
     # Key([mod], "t", lazy.spawn("teams"), desc="Launch MS Teams"),
     Key([mod], "d", lazy.spawn("discord"), desc="Launch Discord"),
+    Key([mod, "shift"], "v", lazy.spawn("virt-manager"), desc="Launch virt-manager"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
