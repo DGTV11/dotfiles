a4c64cdc3d94a37d5b53ae3b836e3511e69c4ad1
Daniel Wee <danielweedk@gmail.com>
2025-03-07 19:09:42 +0800

    Deleted unnecessary nvim keybinds from mappings.lua

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,11 +9,11 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-local wk = require("which-key")
-wk.add({
-    mode = { "v" },
-    { "<leader>s",  group = "Silicon" },
-    { "<leader>sc", function() require("nvim-silicon").clip() end, desc = "Copy code screenshot to clipboard" },
-    { "<leader>sf", function() require("nvim-silicon").file() end,  desc = "Save code screenshot as file" },
-    { "<leader>ss", function() require("nvim-silicon").shoot() end,  desc = "Create code screenshot" },
-})
+-- local wk = require("which-key")
+-- wk.add({
+--     mode = { "v" },
+--     { "<leader>s",  group = "Silicon" },
+--     { "<leader>sc", function() require("nvim-silicon").clip() end, desc = "Copy code screenshot to clipboard" },
+--     { "<leader>sf", function() require("nvim-silicon").file() end,  desc = "Save code screenshot as file" },
+--     { "<leader>ss", function() require("nvim-silicon").shoot() end,  desc = "Create code screenshot" },
+-- })
