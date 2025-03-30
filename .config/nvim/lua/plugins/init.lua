a7b5af31d17d8b3f7ac14c605dc27b294d5d331e
Daniel Wee <danielweedk@gmail.com>
2025-03-30 15:27:49 +0800

    Updated nvim config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -228,11 +228,19 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    cmd = { "RenderMarkdown" },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
+    config = function()
+      local client = require("obsidian").get_client()
+      client.opts.ui.enable = false
+      local namespaceID = vim.api.nvim_get_namespaces()["ObsidianUI"]
+      vim.api.nvim_buf_clear_namespace(0, namespaceID, 0, -1) -- Clear all the obsidian UI stuff before loading this.
+      require("render-markdown").setup({})
+    end,
     keys = {
       {
         "<leader>mr",
