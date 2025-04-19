12fb61b661db1221c905abc2473f93df71308845
Daniel Wee <danielweedk@gmail.com>
2025-04-19 14:04:50 +0800

    Added more qtile wallpapers

--- install-arch.sh
+++ install-arch.sh
@@ -92,10 +92,14 @@ axel https://i.redd.it/2ksegq0ad9zc1.png -o .config/qtile/wallpapers/distant-hor
 axel https://i.redd.it/n105zgebijoc1.png -o .config/qtile/wallpapers/distant-horizons-wallpaper-vanillagen.png
 cp misc/images/cloudy-quasar-catppuccin-mocha.png .config/qtile/wallpapers/cloudy-quasar-catppuccin-mocha.png
 cp misc/images/two-miffy-and-jigglypuff.png .config/qtile/wallpapers/two-miffy-and-jigglypuff.png
-cp misc/images/near-starter-base-freedom-smp.png .config/qtile/wallpapers/near-starter-base-freedom-smp.png
+cp misc/images/near-starter-base-1-freedom-smp.png .config/qtile/wallpapers/near-starter-base-1-freedom-smp.png
+cp misc/images/near-starter-base-2-freedom-smp.png .config/qtile/wallpapers/near-starter-base-2-freedom-smp.png
+cp misc/images/near-starter-base-3-freedom-smp.png .config/qtile/wallpapers/near-starter-base-3-freedom-smp.png
+cp misc/images/near-starter-base-4-freedom-smp.png .config/qtile/wallpapers/near-starter-base-4-freedom-smp.png
 cp misc/images/near-mesa-biome-freedom-smp.png .config/qtile/wallpapers/near-mesa-biome-freedom-smp.png
 cp misc/images/mountain-view-1-freedom-smp.png .config/qtile/wallpapers/mountain-view-1-freedom-smp.png
 cp misc/images/mountain-view-2-freedom-smp.png .config/qtile/wallpapers/mountain-view-2-freedom-smp.png
+cp misc/images/mountain-view-3-freedom-smp.png .config/qtile/wallpapers/mountain-view-3-freedom-smp.png
 
 ## /etc/pacman.conf
 sudo cp misc/pacman.conf /etc/pacman.conf
