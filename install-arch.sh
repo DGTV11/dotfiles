8cfd5a3edf2f781e3951b95ad85bf37487d34a6c
Daniel Wee <danielweedk@gmail.com>
2025-05-11 21:06:30 +0800

    Added more wallpapers

--- install-arch.sh
+++ install-arch.sh
@@ -108,6 +108,7 @@ cp misc/images/mountain-view-6-freedom-smp.png .config/qtile/wallpapers/mountain
 cp misc/images/mountain-view-7-freedom-smp.png .config/qtile/wallpapers/mountain-view-7-freedom-smp.png
 cp misc/images/mountain-base-1-freedom-smp.png .config/qtile/wallpapers/mountain-base-1-freedom-smp.png
 cp misc/images/mountain-base-2-freedom-smp.png .config/qtile/wallpapers/mountain-base-2-freedom-smp.png
+cp misc/images/mountain-base-3-freedom-smp.png .config/qtile/wallpapers/mountain-base-3-freedom-smp.png
 
 ## /etc/pacman.conf
 sudo cp misc/pacman.conf /etc/pacman.conf
