c09fc66ecf1e1fb6cfdf535ed59723cd52f6b71b
Daniel Wee <danielweedk@gmail.com>
2026-02-08 10:56:25 +0800

    latex yay

--- install-arch.sh
+++ install-arch.sh
@@ -53,7 +53,7 @@ sudo systemctl enable touchegg.service
 
 ## Applications
 echo "Installing CLI applications..."
-yes | sudo pacman -S fastfetch starship less curl wget btop openssh rsync stow fzf playerctl bat lazygit yazi starship zoxide thefuck ffmpeg yt-dlp termdown ncdu unzip zip tar screen ueberzugpp silicon tree-sitter-cli axel trash-cli xdotool tealdeer
+yes | sudo pacman -S fastfetch starship less curl wget btop openssh rsync stow fzf playerctl bat lazygit yazi starship zoxide thefuck ffmpeg yt-dlp termdown ncdu unzip zip tar screen ueberzugpp silicon tree-sitter-cli axel trash-cli xdotool tealdeer texlive
 paru -S pfetch-rs pulseaudio-ctl cava brillo unimatrix
 
 echo "Installing GUI applications..."
